6163ff7733f6a3e41f1d71661494bfd53b7af94e
Sheryl Hohman <sherylhohman@yahoo.com>
2016-05-03 12:30:30 -0700

    re-add again splash/tempsecret.js

---
+++ server/splash/tempsecret.js
@@ -0,0 +1,14 @@
+// export default const GITHUB_API_KEY_SH = {
+//   clientID     : c6804217ea9aa8e9aadc,
+//   clientSecret : 14e74b480d4332139e1938dec65fb5c8ba8dea86
+// }
+// var SECRET = {
+//   id     : 'c6804217ea9aa8e9aadc',
+//   secret : '14e74b480d4332139e1938dec65fb5c8ba8dea86'
+// };
+
+// export default SECRET;
+module.exports = {
+  id     : 'c6804217ea9aa8e9aadc',
+  secret : '14e74b480d4332139e1938dec65fb5c8ba8dea86'
+};
