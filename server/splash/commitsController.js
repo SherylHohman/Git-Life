45086118061eb7ba37613108efbe4942d141a8d1
Sheryl Hohman <sherylhohman@yahoo.com>
2016-05-03 11:42:50 -0700

    WIP updating commitAPI request logic - must commit to switch branches

--- server/splash/commitsController.js
+++ server/splash/commitsController.js
@@ -2,31 +2,49 @@ var request = require('request');
 var fs = require('fs');
 
 const SECRET = require('./tempsecret.js');
-const GITHUB_ROOT = 'https://api.github.com/';
-
 const SECRET_URL  = '&client_id=' + SECRET.id + '&client_secret=' + SECRET.secret;
+
+const GITHUB_ROOT = 'https://api.github.com/';
 
 
 module.exports = function(req, res){
 
+    var oneDay = 86400000;
+    var yesterday = Date.now() - oneDay;
+    var yesterdayStr = yesterday.getFullYear + '-'+ yesterday.getDate + '-' + yesterday.getDay;
+
     var endpoint = 'search/issues?q=';
-    var params = 'react in:body updated:>=2013-02-01';
+    var keyword = 'react in:body';
+    var since = 'updated:<=' + yesterdayStr; //+ today.toISOString();
+    var params = keyword + since;
 
-    request({
-      uri: GITHUB_ROOT + endpoint + params,
-      method: 'GET',
-      headers: {'user-agent': 'node.js'}
-      }, function (error, response, body) {
-          if(error){
-            console.log('Error: ', error);
-          }
+      // use the public API for now instead
+      // uri:     GITHUB_ROOT + endpoint + params, // + SECRET_URL,
+    request(
+      {
+        uri:     GITHUB_ROOT + endpoint + params,
+        method:  'GET',
 
-          fs.writeFile(__dirname + '/../storage/commitsDataTemp.txt', JSON.stringify(body), (err) => {
-            if(err){
-              console.log('fs.writeFile error in server/splash/commitsController.js', err);
-            }
-          timeOfLastGitRequest = new Date();
-        });
+        headers: {'user-agent': 'node.js'}
+      },
+      function (error, response, body) {
+        if(error){
+          console.log('error in server/splash/commitsController.js: ', error);
+        }
+
+        // code from saveResultsToFile goes here
+
         res.send(JSON.parse(body).items);
-      });
+      }
+    );
+
+
+      function saveResultsToFile(){
+        fs.writeFile(__dirname + '/../storage/commitsDataTemp.txt', JSON.stringify(body), (err) => {
+          if(err){
+            console.log('fs.writeFile error in server/splash/commitsController.js', err);
+          }
+        });
+      }
+
   }
