2f3f08b489a5fd1d9cadf02b2398e2b4c96e4bfb
Sheryl Hohman <sherylhohman@yahoo.com>
2016-05-03 01:32:40 -0700

    bug: fix typo, and minor edits

--- client/components/commititem.jsx
+++ client/components/commititem.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class CommitItem extends Component{
   render(){
     return (
-      <div>
+      <div className="commitData">
         <p>Issue created At: {this.props.commitTime} <a href={this.props.repo_url}> {this.props.repo_url} </a></p>
         <p>Tags: {this.props.tags}</p>
         <br />
--- client/index.jsx
+++ client/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
-
 /* browserHistory specifies which part of the URL react cares about */
 import {Router, browserHistory} from 'react-router';
 
+import configureStore from './store/configureStore';
 import routes from './routes';
 
 const store = configureStore();
